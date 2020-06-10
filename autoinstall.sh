e2a490ef3123fe9f3a43715556887972e6a5f453
Dmitriy Maksakov <mksvdmtr@yandex.ru>
2020-06-10 15:38:50 +0300

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-echo "\e[92m\e[1m Add $USER to sudoers"
+echo -e "\e[92m\e[1m Add $USER to sudoers"
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-echo "\e[92m\e[1m Make swap"
+echo -e "\e[92m\e[1m Make swap"
 sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
 sudo chmod 700 /swapfile
 sudo mkswap /swapfile
-echo "/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab
+echo -e "/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab
 sudo swapon /swapfile
-echo "\e[92m\e[1m Upgrade packages"
+echo -e "\e[92m\e[1m Upgrade packages"
 sudo pacman-mirrors -c Russia
 sudo pacman -Syyu --noconfirm
-echo "\e[92m\e[1m Install packages from pacman"
+echo -e "\e[92m\e[1m Install packages from pacman"
 sudo pacman -S base-devel yay php nodejs npm yarn postgresql pgadmin4 go vim atom --noconfirm
-echo "\e[92m\e[1m Install packages from yay"
+echo -e "\e[92m\e[1m Install packages from yay"
 yay -S google-chrome rbenv composer deployer sublime-text-3-imfix --noconfirm
-echo "\e[92m\e[1m Configure keyboard lang and panel label"
+echo -e "\e[92m\e[1m Configure keyboard lang and panel label"
 cp -v ./keyboard-layout.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
 cp -v ./xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 clear
-echo "\e[92m\e[1m Rebooting system"
+echo -e "\e[92m\e[1m Rebooting system"
 sleep 3
 clear
-echo "\e[92m\e[1m Bye Bye :)"
+echo -e "\e[92m\e[1m Bye Bye :)"
 sleep 1
 sudo reboot
 
