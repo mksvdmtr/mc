ce3b0644880b64ae08dc1977c464b4586f7c51ba
Dmitriy Maksakov <mksvdmtr@yandex.ru>
2020-06-10 11:51:23 +0300

    fix .config path

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,8 +4,8 @@ sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
 sudo chmod 700 /swapfile
 sudo mkswap /swapfile
 sudo echo "/swapfile none swap defaults 0 0" >> /etc/fstab
-cp ./keyboard-layout.xml .config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
-cp ./xfce4-panel.xml .config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+cp ./keyboard-layout.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
+cp ./xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 
 sudo pacman-mirrors -c Russia
 sudo pacman -Syyu --noconfirm
