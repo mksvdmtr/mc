1ee694b5fe3a64364341f20d17108447578a45f5
Dmitriy Maksakov <mksvdmtr@yandex.ru>
2020-06-18 21:02:43 +0300

    fix bugs

--- autoinstall.sh
+++ autoinstall.sh
@@ -9,51 +9,36 @@ function message() {
 }
 
 function ruby_env() {
-	message "Ruby env"
+	message "Installing ROR env"
 	yay -S rbenv ruby-build --noconfirm
 	RUBY_VERSION=$(rbenv install -l 2>/dev/null | head -1)
 	message "Installing ruby-$RUBY_VERSION"
-	echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-	source ~/.bashrc
+	echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
+	source $HOME/.bashrc
 	rbenv install $RUBY_VERSION
 	rbenv local $RUBY_VERSION
 	gem install bundler
 }
 
 function php_env() {
-	message "PHP env"
-	sudo pacman -S oniguruma tidy libzip --noconfirm
-	yay -S composer deployer --noconfirm
-	curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer | bash
-	echo '
-	export PHPENV_ROOT="/home/rsu/.phpenv"
-	if [ -d "${PHPENV_ROOT}" ]; then
-		  export PATH="${PHPENV_ROOT}/bin:${PATH}"
-		    eval "$(phpenv init -)"
-	fi' >> ~/.bash_profile
-
-	export PHPENV_ROOT="/home/rsu/.phpenv"
-	export PATH="${PHPENV_ROOT}/bin:${PATH}"
-	eval "$(phpenv init -)"
-	phpenv install 7.4.7
-}
-
-function php_brew() {
+	message "Installing PHP env"
+	sudo -S mariadb apache php-apache --noconfirm
 	yay -S phpbrew --noconfirm
 	echo "extension=bz2.so" | sudo tee -a /etc/php/php.ini
         phpbrew init
+	echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> $HOME/.bashrc
 	phpbrew install 7.4	
 }
 
 paths=(
-	"./dconf ~/.config/" 
-	"./plank ~/.config/"
-	"./plank.desktop ~/.config/autostart/plank.desktop"
-	"./xfwm4.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
-	"./xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
-	"./xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
-	"./keyboard-layout.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml"
-	"./xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
+	"./dconf $HOME/.config/" 
+	"./plank $HOME/.config/"
+	"./plank.desktop $HOME/.config/autostart/plank.desktop"
+	"./xfwm4.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml"
+	"./xfce4-keyboard-shortcuts.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
+	"./xfce4-desktop.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
+	"./keyboard-layout.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml"
+	"./xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
 )
 
 message "Adding $USER to sudoers"
@@ -68,7 +53,7 @@ message "Upgrading packages"
 sudo pacman-mirrors -c Russia
 sudo pacman -Syyu --noconfirm
 message "Installing packages from pacman"
-sudo pacman -S base-devel yay nodejs npm yarn postgresql go vim code plank ntp --noconfirm
+sudo pacman -S base-devel yay nodejs npm yarn go vim code plank ntp --noconfirm
 message "Setting time-zone"
 sudo timedatectl set-timezone Europe/Moscow
 sudo timedatectl set-ntp true
@@ -80,11 +65,12 @@ case $DEV_ENV in
 	ruby_env
       ;;
     php)
-	php_brew
+	php_env
       ;;
+esac
 
 message "Configuring keyboard lang and panel label"
-rm ~/.config/plank/dock1/launchers/*
+rm $HOME/.config/plank/dock1/launchers/*
 
 for p in ${paths[@]}; do
 	cp -rv $p
