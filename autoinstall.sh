0ed94e3dbf35b485e58c5faf3e262acea8431655
Dmitriy Maksakov <mksvdmtr@yandex.ru>
2020-06-18 09:53:43 +0300

    message in func

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
+
+function message() {
 printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Adding $USER to sudoers\e[0m"
+echo -e "\e[92m\e[1m $1 \e[0m"
 printf "%$(tput cols)s\n"|sed "s/ /#/g"
+
+}
+
+message "Adding $USER to sudoers"
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Making swap\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Making swap"
 sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
 sudo chmod 700 /swapfile
 sudo mkswap /swapfile
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
 echo -e "/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
 sudo swapon /swapfile
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Upgrading packages\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Upgrading packages"
 sudo pacman-mirrors -c Russia
 sudo pacman -Syyu --noconfirm
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Installing packages from pacman\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Installing packages from pacman"
 sudo pacman -S base-devel yay php nodejs npm yarn postgresql go vim code plank ntp --noconfirm
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Setting time-zone\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Setting time-zone"
 sudo timedatectl set-timezone Europe/Moscow
 sudo timedatectl set-ntp true
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Installing packages from yay\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Installing packages from yay"
 yay -S google-chrome rbenv ruby-build composer deployer sublime-text-3-imfix dbeaver plank-theme-arc flameshot --noconfirm
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
 RUBY_VERSION=$(rbenv install -l 2>/dev/null | head -1)
-echo -e "\e[92m\e[1m Installing ruby-$RUBY_VERSION\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Installing ruby-$RUBY_VERSION"
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 rbenv install $RUBY_VERSION
 rbenv local $RUBY_VERSION
 gem install bundler
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Configuring keyboard lang and panel label\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Configuring keyboard lang and panel label"
 cp -rv ./dconf ~/.config/
 rm ~/.config/plank/dock1/launchers/*
 cp -rv ./plank ~/.config/
@@ -52,11 +42,6 @@ cp -v ./xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 cp -v ./xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 cp -v ./keyboard-layout.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
 cp -v ./xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
-echo -e "\e[92m\e[1m Rebooting system ...\e[0m"
-printf "%$(tput cols)s\n"|sed "s/ /#/g"
+message "Rebooting system ..."
 sleep 3
-echo -e "\e[92m\e[1m Bye Bye :)\e[0m"
-sleep 1
 sudo reboot
-
